229e0bbc49118570fd28270e82294ae742ba4d47
Adam Robson <adamray312@gmail.com>
2024-01-21 15:16:32 -0800

    render the dark and light logo based on the os color scheme

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,15 +1,22 @@
+'use client'
+import { useEffect, useState } from 'react';
+import darkLogo from '../../public/images/ew_logo_dk.svg'
+import lightLogo from '../../public/images/ew_logo.svg'
 import Image from 'next/image';
 import { LogoProps } from '../../lib/types'
 
 export default function Logo({ height, width }: LogoProps) {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+  
   return (
-    <Image
-      src="/images/ew_logo_disco.svg"
-      alt="ellington willoughby logo"
-      height={height}
-      width={width}
-      className=""
-      priority={true}
-    />
+    <div>
+      <Image
+        src={isDarkMode ? darkLogo : lightLogo}
+        alt="ellington willoughby logo"
+        height={height}
+        width={width}
+        priority={true}
+      />
+    </div>
   )
 }
