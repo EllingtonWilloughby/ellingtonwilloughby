50e719f1c0b3d1288daec63c7da0fbc4a61a84c4
Adam Robson <adamrayrobson@gmail.com>
2024-09-27 00:33:50 -0700

    update useAudio hook
    Signed-off-by: Adam Robson <adamrayrobson@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,29 @@
-import type { Metadata } from 'next'
-import { Cairo_Play } from 'next/font/google'
-import './globals.css'
-import { DarkModeProvider } from '@/lib/context/ColorSchemeContext'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+import type { Metadata } from 'next';
+import { Cairo_Play } from 'next/font/google';
+import './globals.css';
+import { ColorSchemeProvider } from '@/lib/context/ColorSchemeContext';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
-const cairo = Cairo_Play({ subsets: ['latin'] })
+const cairo = Cairo_Play({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Ellington Willoughby',
-  description: 'Ellington Willoughby & the Mythical Squid',
-  icons: [
-    '/images/ew_logo.svg',
-    '/images/ew_logo_dk.svg',
-  ]
-}
+  description: 'Ellington Willoughby & the Mythical Squid'
+};
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <DarkModeProvider>
+      <ColorSchemeProvider>
         <body className={cairo.className}>
           {children}
           <SpeedInsights />
         </body>
-      </DarkModeProvider>
+      </ColorSchemeProvider>
     </html>
-  )
+  );
 }
--- lib/hooks/useAudio.tsx
+++ lib/hooks/useAudio.tsx
@@ -1,112 +1,121 @@
-'use client'
-import { useEffect, useRef, useState } from 'react'
-import { Howl } from 'howler'
-import { playlist } from '../data'
+'use client';
+import { useEffect, useRef, useState } from 'react';
+import { Howl } from 'howler';
+import { playlist } from '../data';
 
 export function useAudio() {
-  const [isPlaying, setIsPlaying] = useState<boolean>(false)
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [songIndex, setSongIndex] = useState<number>(0);
+  const [elapsedTime, setElapsedTime] = useState<string>('00:00');
+  const [volume, setVolume] = useState<number>(0.5);
+  const [isMuted, setIsMuted] = useState<boolean>(false);
 
-  const [songIndex, setSongIndex] = useState<number>(0)
-
-  const [seek, setSeek] = useState<number>(0)
-  const [elapsedTime, setElapsedTime] = useState<string>('00:00')
-
-  const [volume, setVolume] = useState<number>(0.5)
-  const [volumeBeforeMute, setVolumeBeforeMute] = useState<number>(0)
-  const [isMuted, setIsMuted] = useState<boolean>(false)
-
-  const sound = useRef<Howl | null>(null)
+  const sound = useRef<Howl | null>(null);
 
   useEffect(() => {
+    try {
+      const initializeHowler = () => {
+        sound.current = new Howl({
+          preload: 'metadata',
+          autoplay: isPlaying,
+          src: [playlist[songIndex].url],
+          volume: 0.5,
+          onend: () => handleNextSong()
+        });
 
-    function initializeHowler() {
-      sound.current = new Howl({
-        preload: 'metadata',
-        autoplay: isPlaying,
-        src: [playlist[songIndex].url],
-        volume: 0.5,
-        onend: () => {
-          handleNextSong()
+        if (isPlaying) {
+          sound.current?.play();
         }
-      });
+      };
 
-      if (isPlaying) {
-        sound.current?.seek(seek);
-      }
+      initializeHowler();
+    } catch (error) {
+      if (error instanceof Error)
+        console.error(
+          `There was an error while initializing Howler: ${error.message}`
+        );
     }
-    initializeHowler();
 
     return () => {
       sound.current?.unload();
     };
-  }, [seek, songIndex, isPlaying]);
+  }, [songIndex, isPlaying, playlist]);
 
   useEffect(() => {
     if (!isMuted) {
-      sound.current?.volume(volume)
+      sound.current?.volume(volume);
     } else {
-      sound.current?.volume(0)
+      sound.current?.volume(0);
     }
-  }, [volume, isMuted])
+  }, [volume, isMuted]);
 
   useEffect(() => {
-  if (isPlaying) {
-    const interval = setInterval(() => {
-      const seconds = Math.floor(sound.current?.seek() as number);
-      const minutes = Math.floor(seconds / 60);
-      const remainingSeconds = seconds % 60;
-      setElapsedTime(`${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`);
-    }, 1000);
-    return () => clearInterval(interval);
-  }
-}, [isPlaying]);
-
-  function handlePlayPause() {
-    if (!isPlaying) {
-      sound.current?.play()
-      setIsPlaying(true)
+    if (isPlaying) {
+      const interval = setInterval(() => {
+        const seconds = Math.floor(sound.current?.seek() as number);
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        setElapsedTime(
+          `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`
+        );
+      }, 1000);
+      return () => clearInterval(interval);
     } else {
-      sound.current?.pause()
-      setIsPlaying(false)
-      setSeek(sound.current?.seek() as number)
+      setElapsedTime('00:00');
+    }
+  }, [isPlaying]);
+
+  function handlePlayPause(): void {
+    if (!sound.current) return;
+
+    if (!isPlaying) {
+      sound.current?.play();
+      setIsPlaying(true);
+    } else {
+      sound.current?.pause();
+      setIsPlaying(false);
     }
   }
 
-  function handleNextSong() {
-    setSongIndex((prevIndex) =>
+  function handleNextSong(): void {
+    sound.current?.stop();
+    setIsPlaying(false);
+    setSongIndex((prevIndex: number) =>
       prevIndex === playlist.length - 1 ? 0 : prevIndex + 1
     );
+    sound.current?.play();
     setIsPlaying(true);
-    setSeek(0)
   }
 
-  function handlePrevSong() {
+  function handlePrevSong(): void {
+    sound.current?.stop();
+    setIsPlaying(false);
     setSongIndex((prevIndex) =>
       prevIndex === 0 ? playlist.length - 1 : prevIndex - 1
     );
+    sound.current?.play();
     setIsPlaying(true);
-    setSeek(0);
   }
 
   function handleVolumeChange(event: React.ChangeEvent<HTMLInputElement>) {
-    event.preventDefault()
-    const newVolume = parseFloat(event.target.value)
-    setVolume(newVolume)
-    setVolumeBeforeMute(newVolume)
-    sound.current?.volume(newVolume)
+    event.preventDefault();
+    const newVolume = parseFloat(event.target.value);
+    setVolume(newVolume);
+    sound.current?.volume(newVolume);
   }
 
   function toggleMute() {
-    if (!isMuted) {
-      setVolumeBeforeMute(volume)
-      setVolume(0)
-    } else {
-      setVolume(volumeBeforeMute)
-    }
-    setIsMuted((prevIsMuted) => !prevIsMuted)
+    setIsMuted((prevIsMuted: boolean) => {
+      if (!prevIsMuted) {
+        sound.current?.volume(0);
+      } else {
+        sound.current?.volume(volume);
+      }
+      return !prevIsMuted;
+    });
   }
 
-  const currentSong = playlist[songIndex]
+  const currentSong = playlist[songIndex];
 
   return {
     elapsedTime,
@@ -118,5 +127,5 @@ export function useAudio() {
     handleVolumeChange,
     toggleMute,
     currentSong
-  }
+  };
 }
