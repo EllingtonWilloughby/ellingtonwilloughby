875a2da15fe01b528d34777911ff3c380a863161
Adam Robson <adamrayrobson@gmail.com>
2024-10-03 11:44:25 -0700

    update shows page
    Signed-off-by: Adam Robson <adamrayrobson@gmail.com>

--- app/shows/page.tsx
+++ app/shows/page.tsx
@@ -1,52 +1,24 @@
 'use client';
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
-import { useDarkMode } from '@/lib/context/ColorSchemeContext';
-import { PiHouseFill } from 'react-icons/pi';
+import { House } from '@phosphor-icons/react';
 
-export default function Shows() {
-  const { darkMode } = useDarkMode();
-
+export default function ShowsPage() {
   return (
-    <div className="relative min-h-screen max-w-screen-md w-full mx-auto flex flex-col justify-start items-center p-6">
-      {darkMode
-          ? (<Image src="/images/shows_dk.svg" height={500} width={500} alt="shows heading" className="-mt-20 -mb-20 pt-6" />)
-          : (<Image src="/images/shows.svg" height={500} width={500} alt="shows heading" className="-mt-20 -mb-20 pt-6" />)}
-      <Link href="/" className="absolute top-8 left-8 text-sm"><PiHouseFill size="24" /></Link>
-      <div className="w-full max-w-screen-sm mx-auto mt-10 flex justify-center">
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Time</th>
-              <th>Location</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="text-xs subpixel-antialiased">
-                05-18-2024
-              </td>
-              <td className="text-xs subpixel-antialiased">7 PM</td>
-              <td className="text-xs subpixel-antialiased">
-                Starday Tavern 6517 SE Foster Rd, Portland, OR 97206
-              </td>
-            </tr>
-            <tr>
-              <td className="text-xs subpixel-antialiased">
-                TBD
-              </td>
-              <td className="text-xs subpixel-antialiased">TBD</td>
-              <td className="text-xs subpixel-antialiased">
-                Buddystock 1997 State Road, Mosier, OR 97040
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-       <Image src="/images/live.jpeg" height={500} width={500} alt="image of the wall in the studio" className="my-10 rounded-lg" />
-      </div>
-  );
+    <div className="relative min-h-screen w-full max-w-screen-xl mx-auto p-2">
+      <section className="relative max-h-40 w-full">
+        <Link href="/" className="link-container absolute top-4 left-4">
+          <House weight="duotone" size={24} />
+          <span className="link-label absolute top-2 left-8">Home</span>
+        </Link>
+        <h2 className="min-h-20 w-full text-center text-3xl md:text-4xl lg:text-5xl subpixel-antialiased p-10">
+          Shows
+        </h2>
+      </section>
 
+      <div className="w-full max-w-screen-md mx-auto flex flex-col justify-between items-center flex-auto rounded-lg shadow-lg backdrop-blur-xl backdrop-saturate-50 p-4 min-h-96">
+        Coming Soon...
+      </div>
+    </div>
+  );
 }
