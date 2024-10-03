d739f3bc4355939881c560bc5ef8ec249cebfc23
Adam Robson <adamrayrobson@gmail.com>
2024-10-03 10:44:13 -0700

    light mode mobile
    Signed-off-by: Adam Robson <adamrayrobson@gmail.com>

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -10,14 +10,14 @@ export default function MusicPage() {
       <section className="relative max-h-40 w-full">
         <Link href="/" className="link-container absolute top-4 left-4">
           <House weight="duotone" size={24} />
-          <span className="link-label absolute top-4 left-6">Home</span>
+          <span className="link-label absolute top-2 left-8">Home</span>
         </Link>
         <h2 className="min-h-20 w-full text-center text-3xl md:text-4xl lg:text-5xl subpixel-antialiased p-10">
           Music
         </h2>
       </section>
 
-      <section className="w-full">
+      <section className="w-full max-w-screen-md mx-auto flex flex-col justify-between items-center flex-auto rounded-lg shadow-lg backdrop-blur-xl backdrop-saturate-50 p-4">
         <Albums />
       </section>
     </div>
