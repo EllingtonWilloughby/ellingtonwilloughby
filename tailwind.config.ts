baded5a8597012e17a01a9f577a694fef1d49d10
Adam Robson <adamrayrobson@gmail.com>
2024-10-06 01:34:44 -0700

    add cssnano

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import postcss from 'postcss';
 
 const config: Config = {
   content: [
@@ -13,11 +14,15 @@ const config: Config = {
         xs: '512px'
       },
       colors: {
+        root: 'var(--root)',
+        counterpart: 'var(--counterpart)',
         text: 'var(--text)',
         background: 'var(--background)',
         primary: 'var(--primary)',
         secondary: 'var(--secondary)',
-        accent: 'var(--accent)'
+        accent: 'var(--accent)',
+        hover: 'var(--hover)',
+        active: 'var(--active)'
       }
     }
   },
