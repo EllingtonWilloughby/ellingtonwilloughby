8708f58dc1d64cf96c749a23e71f1b6fa889e0da
Adam Robson <adamrayrobson@gmail.com>
2024-11-19 20:28:36 -0800

    update audio player component

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -7,15 +7,13 @@ import Image from "next/image";
 export default function Logo() {
   const { theme } = useThemeContext();
   return (
-    <div className="mx-auto flex justify-center items-start max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg">
-      <Image
-        src={theme ? "/images/logo_i_light.svg" : "/images/logo_i_dark.svg"}
-        alt="Ellington Willoughby & the Mythical Squid band logo"
-        width={440}
-        height={440}
-        className="w-full min-w-96"
-        priority
-      />
-    </div>
+    <Image
+      src={theme ? "/images/logo_i_dark.svg" : "/images/logo_i_light.svg"}
+      alt="Ellington Willoughby & the Mythical Squid band logo"
+      width={480}
+      height={480}
+      className="w-full max-w-screen-sm mx-auto"
+      priority
+    />
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,13 @@ export default function Home() {
   return (
     <div className="relative w-full h-full flex flex-col justify-start items-center">
       <Navigation />
+
       <Logo />
+
       <AudioProvider initialIndex={0} initialVolume={0.5}>
         <AudioPlayer />
       </AudioProvider>
+
       <div className="w-full max-w-screen-lg mx-auto fixed bottom-4">
         <Icon />
       </div>
